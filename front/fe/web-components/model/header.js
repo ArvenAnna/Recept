50188ff50ed35cef16c4764ad3ac067526a15e07
hanna <hanna@example.com>
2019-11-07 21:15:52 +0200

    fix bug with translations

--- front/fe/web-components/model/header.js
+++ front/fe/web-components/model/header.js
@@ -1,7 +1,5 @@
 import Model from '../abstract/model';
 import mTranslations from "./translations";
-import mRecipeSearch from "./recipeSearch";
-import mDepartments from "./departments";
 
 const NEW_RECIPE_ID = 1;
 const INGREDIENTS_ID = 2;
@@ -19,15 +17,13 @@ class Header extends Model {
     constructor() {
         super();
 
-        mTranslations.addSubscriber(this._setTranslations);
-
         this.$menu = {
-            NEW_RECIPE: { trans: mTranslations.getTranslation('common.new_recipe'), id: NEW_RECIPE_ID, linkFn: () => '/recipe', active: true },
-            INGREDIENTS: { trans: mTranslations.getTranslation('common.ingredients'), id: INGREDIENTS_ID, linkFn: () =>'/ingredients', active: true },
-            NEW_INGREDIENT: { trans: mTranslations.getTranslation('common.new_ingredient'), id: NEW_INGREDIENT_ID, linkFn: () =>'/ingredient', active: true },
-            EDIT_RECIPE: { trans: mTranslations.getTranslation('common.edit_recipe'), id: EDIT_RECIPE_ID, linkFn: (id) => `/recipe/${id}/edit`, active: false},
-            EDIT_INGREDIENT:  { trans: mTranslations.getTranslation('common.edit_ingredient'), id: EDIT_INGREDIENT_ID, linkFn: (id) => `/ingredients/${id}/edit`, active: false},
-            MENU: {trans: mTranslations.getTranslation('common.menu'), id: MENU_ID, linkFn: () => `/menu`, active: true}
+            NEW_RECIPE: { trans: () => mTranslations.getTranslation('common.new_recipe'), id: NEW_RECIPE_ID, linkFn: () => '/recipe', active: true },
+            INGREDIENTS: { trans: () => mTranslations.getTranslation('common.ingredients'), id: INGREDIENTS_ID, linkFn: () =>'/ingredients', active: true },
+            NEW_INGREDIENT: { trans: () => mTranslations.getTranslation('common.new_ingredient'), id: NEW_INGREDIENT_ID, linkFn: () =>'/ingredient', active: true },
+            EDIT_RECIPE: { trans: () => mTranslations.getTranslation('common.edit_recipe'), id: EDIT_RECIPE_ID, linkFn: (id) => `/recipe/${id}/edit`, active: false},
+            EDIT_INGREDIENT:  { trans: () => mTranslations.getTranslation('common.edit_ingredient'), id: EDIT_INGREDIENT_ID, linkFn: (id) => `/ingredients/${id}/edit`, active: false},
+            MENU: {trans: () => mTranslations.getTranslation('common.menu'), id: MENU_ID, linkFn: () => `/menu`, active: true}
 
         };
 
@@ -38,29 +34,27 @@ class Header extends Model {
         this._initButtons = this._initButtons.bind(this);
         this._setTranslations = this._setTranslations.bind(this);
 
+        mTranslations.addSubscriber(this._setTranslations);
+
         this._initButtons();
     }
 
     async _setTranslations() {
-        const translations = await Promise.all(Object.values(this.$menu).map(v => v.trans));
+        const translations = await Promise.all(Object.values(this.$menu).map(v => {
+            return v.trans();
+        }));
         Object.values(this.$menu).forEach((value, i) => {
             value.name = translations[i];
         });
+        this.notifySubscribers();
     }
 
     async _initButtons() {
-        // const translations = await Promise.all(Object.values(this.$menu).map(v => v.trans));
-
-        await this._setTranslations();
-        // Object.values(this.$menu).forEach((value, i) => {
-        //     value.name = translations[i];
-        // });
-
         this.$menu.NEW_RECIPE.to = this.$menu.NEW_RECIPE.linkFn();
         this.$menu.INGREDIENTS.to = this.$menu.INGREDIENTS.linkFn();
         this.$menu.NEW_INGREDIENT.to = this.$menu.NEW_INGREDIENT.linkFn();
         this.$menu.MENU.to = this.$menu.MENU.linkFn();
-        this.notifySubscribers();
+        await this._setTranslations();
     }
 
     addRecipeEditButton(id) {
